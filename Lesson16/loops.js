b8598af63ee8dba257d63c210abee19f6dffdfad
Anna S. A <anna.atalay@minna.tech>
2023-06-13 14:36:18 +0300

    Add Lesson16 files

---
+++ Lesson16/loops.js
@@ -0,0 +1,38 @@
+// 1. Write a loop that prints out the numbers from 1 to 10.
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 2. Write a loop that prints out the odd numbers from 1 to 20.
+// 1,3,5,7...
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 3. Write a loop that prints out the even numbers from 1 to 20.
+// 2, 4, 6...
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 4. Write a loop that finds the maximum number in an array of numbers.
+const findMaxNumberInArray = [3, 10, 23, 42, 55, 12, 7, 4, 90];
+
+console.log('- - - - - - - - - - - - - - - - - ');
+
+// 5. Write a loop that finds the minimum number in an array of numbers.
+const findMinNumberInArray = [10, 1, 23, 42, 55, 12, 7, 4];
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 6. Write a loop that reverses a given string.
+const string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.';
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 7. Write a while loop that counts from 10 to 1 and prints out each number.
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 8. Write a while loop that generates random numbers between 1 and 10 until a number greater than 8 is generated, and then prints out the total number of iterations.
+
+console.log('- - - - - - - - - - - - - - - - - ');
+// 9. Write a do-while loop that counts from 1 to 10 and prints out each number.
+
+// 10. Write a for...in loop that prints out the keys and values of an object.
+let person = {name: "John", surname: "Doe", age: 30, city: "New York"};
+
+
+// 11. Write a for...in loop that sums the values of an object.
+let obj = {a: 10, b: 20, c: 30};
